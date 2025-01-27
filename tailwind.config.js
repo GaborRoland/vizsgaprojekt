25adbfd19cdfea2fa68c6ee959f713569b318eb3
Szili Nóra <168283580+sznora@users.noreply.github.com>
2025-01-27 10:55:10 +0100

    Tailwind css konfigurálva sikeresen.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+       './login/index.html',
+        './login/src/*.css',
+      ],
+    theme: {
+    },
+    plugins: [],
+  }
